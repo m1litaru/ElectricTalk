2708e272874c6204cc736f9d2270357948ce3169
m1litaru <militaru.adrian25@gmail.com>
2019-11-24 00:19:22 +0200

    added gravatar and edit account info

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { Storage } from '@ionic/storage';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Md5 } from 'ts-md5/dist/md5';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +14,9 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+  public gravatarUrl: string = "";
+  public userName: string = "";
+  public link: string = "https://s.gravatar.com/avatar/1c3245e1fc158558fdae158bd52355d2?s=80";
   public appPages = [
     {
       title: 'User',
@@ -41,6 +46,7 @@ export class AppComponent {
     private statusBar: StatusBar,
     private storage: Storage,
     private http: HttpClient,
+    private navCtrl: NavController
   ) {
     this.initializeApp();
   }
@@ -49,11 +55,11 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.obtainGravatar();
     });
   }
 
   obtainGravatar() {
-    // console.log(md5("dnielhort@gmail.com"));
     this.storage.get("token")
     .then(token => {
       let header_register = {
@@ -66,11 +72,27 @@ export class AppComponent {
 
       this.http.get("/api/Auth/Me", header_register)
         .subscribe(data => {
-          console.log(data);
+
+          if(data['firstName'] != undefined && data['lastName'] != undefined) {
+            // this.userName = data['firstName'].toString() + " " + data['lastName'].toString();
+            this.storage.set("name", data['firstName'].toString() + " " + data['lastName'].toString());
+            this.storage.get("name").then( name => {
+              this.userName = name;
+            })
+          }
+          
+          if(data['email'] != undefined) {
+            this.gravatarUrl = "https://www.gravatar.com/avatar/" + Md5.hashStr(data['email']).toString();
+            console.log(this.gravatarUrl);
+          }
         }, error => {
         console.log(error);
       });
     });
+  }
+  
+  goToPage(path: string){
+    this.navCtrl.navigateForward(path);
   }
   
 }
